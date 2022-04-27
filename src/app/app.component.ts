dbe21a80af97fdebcf8b5af77ff120bd0af16f3f
Mahmoud AlAsad <mahmoud.alasad@csuglobal.edu>
2022-04-28 01:40:19 +0400

    minor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   
-    constructor() { }
+    constructor(private router: Router) { }
 
+    logout(){
+      this.router.navigate(['/welcome']);
+    }
 }
